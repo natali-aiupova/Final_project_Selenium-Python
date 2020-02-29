854ff316a136b214aab1b977b77b6ef5d2a40ddc
natali-aiupova <aiupova.na@gmail.com>
2020-02-29 20:02:27 +0700

    Add locators to add products to cart

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,11 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_NAME_MESSAGE = (By.CSS_SELECTOR, ".alert-success:nth-child(1) strong")
+    PRODUCT_PRICE_MESSAGE = (By.CSS_SELECTOR, ".alertinner p strong")
+
