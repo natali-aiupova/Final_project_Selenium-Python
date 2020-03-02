0a8d7f3991615f83fb7d784ec23e0fc7079dece8
natali-aiupova <aiupova.na@gmail.com>
2020-03-02 17:24:05 +0700

    Add tests check login link & click

--- test_product_page.py
+++ test_product_page.py
@@ -46,3 +46,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.open()
     product_page.add_product_to_cart()
     product_page.should_be_out_success_message()
+
+# Гость видит ссылку перехода на страницу логина
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+# Гость может перейти на страницу логина со страницы Х
+def test_guest_can_go_to_login_page_from_product_page(browser): 
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
