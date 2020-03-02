13b6d0ce5a1624210e9809cde6d0baaac991ad4c
natali-aiupova <aiupova.na@gmail.com>
2020-03-02 16:52:37 +0700

    Add a stub, remove methods

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,13 +3,7 @@ from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 from .login_page import LoginPage
 
+# В классе MainPage у нас не осталось никаких методов, поэтому добавим заглушку
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        # return LoginPage(browser=self.browser, url=self.browser.current_url)
-        alert = self.browser.switch_to.alert
-        alert.accept()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
