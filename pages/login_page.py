df601a58cad409d3e14a97366ef3661a067456cb
natali-aiupova <aiupova.na@gmail.com>
2020-03-03 21:13:22 +0700

    Add method register new user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,18 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
-
 class LoginPage(BasePage):
+    # Регистрация нового пользователя
+    def register_new_user(self, email, password):
+        register_email = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        register_email.send_keys(email)
+        register_password = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD)
+        register_password.send_keys(password)
+        confirm_password = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD)
+        confirm_password.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
