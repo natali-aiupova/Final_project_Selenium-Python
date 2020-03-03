19887046e698a81aa10e864f6046922f1e2aeed7
natali-aiupova <aiupova.na@gmail.com>
2020-03-03 18:27:28 +0700

    Add locators for register user

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,10 @@ class LoginPageLocators():
     LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_SUBMIT = (By.CSS_SELECTOR, ".register_form .btn")
 
 class ProductPageLocators():
     ADD_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
