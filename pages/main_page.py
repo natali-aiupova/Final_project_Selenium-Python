2b3bb3e78619d6c2c5b1f4a3c3a06e10ab4ba0f0
natali-aiupova <aiupova.na@gmail.com>
2020-03-04 15:27:09 +0700

    Delete extra lines, add comments

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,8 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
-from .locators import BasePageLocators
-from .login_page import LoginPage
 
 # В классе MainPage у нас не осталось никаких методов, поэтому добавим заглушку
 class MainPage(BasePage):
     def __init__(self, *args, **kwargs):
         super(MainPage, self).__init__(*args, **kwargs)
+
+# Метод __init__ вызывается при создании объекта. Конструктор выше с ключевым словом super только вызывает конструктор класса предка и передает ему все те аргументы, которые мы передали в конструктор MainPage.
