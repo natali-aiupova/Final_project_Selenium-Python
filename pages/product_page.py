273d01214b989015f5df67cd6ba384393607d25e
natali-aiupova <aiupova.na@gmail.com>
2020-03-04 15:28:56 +0700

    Add comments

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,26 +7,26 @@ class ProductPage(BasePage):
         button_add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_BASKET_BUTTON)
         button_add_to_basket.click()
 
-    # Вызываем проверки.
+    # Вызываем проверки 1) и 2).
     def should_be_correct_add_product_to_cart(self):
         self.should_be_correct_product_name()
         self.should_be_correct_product_price()
 
-    # Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
+    # 1) Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
     def should_be_correct_product_name(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         product_name_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_MESSAGE).text
         assert product_name == product_name_message, \
             f"The add product name '{product_name_message}' is not correct. Should be '{product_name}"
 
-    # Стоимость корзины совпадает с ценой товара. 
+    # 2) Стоимость корзины совпадает с ценой товара. 
     def should_be_correct_product_price(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         product_price_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_MESSAGE).text
         assert product_price == product_price_message, \
             f"The price of add product '{product_price_message}' is not correct. Should be '{product_price}"
 
-    # Проверка, что элемент не появляется на странице
+    # Проверка, что на странице нет сообщения об успехе
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
            "Success message is presented, but should not be"
