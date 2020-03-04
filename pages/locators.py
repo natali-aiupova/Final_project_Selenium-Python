000b39fd3f7a58d1752bdc68ac65c055244d607d
natali-aiupova <aiupova.na@gmail.com>
2020-03-04 15:14:45 +0700

    Delete commented lines

--- pages/locators.py
+++ pages/locators.py
@@ -19,9 +19,7 @@ class ProductPageLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a.btn")
     BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner")
     BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
-
