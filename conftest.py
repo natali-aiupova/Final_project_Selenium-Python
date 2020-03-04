88fab562d8d2c67601f606c54a582a7b460c613f
natali-aiupova <aiupova.na@gmail.com>
2020-03-04 13:37:24 +0700

    Arrange code

--- conftest.py
+++ conftest.py
@@ -3,13 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', \
-                     action='store', \
-                     default="chrome", \
+    parser.addoption("--browser_name", action="store", default="chrome", \
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', \
-                     action='store', \
-                     default="en", \
+    parser.addoption("--language", action="store", default="en", \
                      help="Choose language: ru / en / ...")
 
 @pytest.fixture(scope="function")
@@ -19,18 +15,15 @@ def browser(request):
 
     if browser_name == "chrome":
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        print("\nstart chrome browser for test..")
+        options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
         browser = webdriver.Chrome(options=options)
 
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        print("\nstart firefox browser for test..")
         browser = webdriver.Firefox(firefox_profile=fp)
 
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\nquit browser..")
     browser.quit()
