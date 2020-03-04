51e509f97e779c1f45e89c5b35c17fb2eccfe2c9
natali-aiupova <aiupova.na@gmail.com>
2020-03-04 15:34:21 +0700

    Fix name locators

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -6,10 +6,10 @@ class BasketPage(BasePage):
 
     # Проверка, что корзина пуста
     def should_be_empty_basket(self):
-        assert self.is_not_element_present(*BasePageLocators.BASKET_ITEMS), \
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), \
            "There are items in the basket, but should be not"
 
     # Проверка, что есть cообщение о том, что корзина пуста
     def should_be_message_empty_basket(self):
-        assert self.is_element_present(*BasePageLocators.BASKET_MESSAGE), \
+        assert self.is_element_present(*BasketPageLocators.BASKET_MESSAGE), \
            "The basket is not empty, but should be"
